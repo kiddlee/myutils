52ba2ce5d7f48110dfa4cd438b3a7a2442062dcc
lishulei <lishulei@huoyunren.com>
2015-11-18 11:04:28 +0800

    add docker compose

--- linux/init.sh
+++ linux/init.sh
@@ -9,3 +9,6 @@ touch /etc/apt/sources.list.d/docker.list
 echo 'deb https://apt.dockerproject.org/repo ubuntu-trusty main' > /etc/apt/sources.list.d/docker.list
 apt-get update
 apt-get install -y docker-engine
+#install docker compose
+curl -L https://github.com/docker/compose/releases/download/1.5.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
