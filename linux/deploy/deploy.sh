7bb795e9ff247df7510db21ef0a912c052244e24
lishulei <lishulei@huoyunren.com>
2015-11-27 09:02:49 +0800

    add file checking

--- linux/deploy/deploy.sh
+++ linux/deploy/deploy.sh
@@ -5,17 +5,34 @@ workdir=$(dirname `pwd`)
 check_evn() {
     if [ $DEV_PATH ]
     then
-        if [ -d $DEV_PATH ]
-	fi
+        if ! [ -d $DEV_PATH ]
+        then
+            sudo mkdir -p $DEV_PATH 
+        fi
+        sudo mkdir -p ${DEV_PATH}/etc/nginx/conf/sites-enabled
+        sudo mkdir -p ${DEV_PATH}/etc/nginx/conf/certs
+        sudo mkdir -p ${DEV_PATH}/etc/nginx/conf/conf.d
+        sudo mkdir -p ${DEV_PATH}/var/log/nginx
+        sudo mkdir -p ${DEV_PATH}/web
+        sudo mkdir -p ${DEV_PATH}/var/log/php5
+        sudo mkdir -p ${DEV_PATH}/var/log/mysql
+        if ! [ -f etc/nginx/conf/sites-enabled/default ]
+        then
+            sudo cp etc/nginx/conf/sites-enabled/default ${DEV_PATH}/etc/nginx/conf/sites-enabled/
+        fi
     else
         echo "need set DEV_PATH"
+        return 1
     fi
     if [ $DATA_PATH ]
     then
-        if [ -d $DATA_PATH ]
-	fi
+        if ! [ -d $DATA_PATH ]
+        then
+    	   sudo mkdir -p $DATA_PATH 
+        fi
     else
         echo "need set DATA_PATH"
+        return 1
     fi
 }
 
@@ -43,6 +60,7 @@ work() {
 }
 
 up() {
+    check_evn
     docker-compose up -d
     update-docker-dnsmasq
     update_dnsmasq
